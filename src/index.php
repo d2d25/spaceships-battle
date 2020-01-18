3b813d836776aeca49bda5f720eadaa8e02168bd
quentin.picart <quentin.picart@gmail.com>
2020-01-18 08:43:38 +0100

    gestion erreurs sign up form

--- src/index.php
+++ src/index.php
@@ -2,32 +2,41 @@
 include_once('../includes/constants.php');
 include_once('../includes/functions.php');
 
+$errors=[];
 // traitement formulaires si methode POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // traitement form sign up
     if (isset($_POST['sign_up'])) {
-        if (!empty($_POST['user_login']) && !empty($_POST['user_password'])) {
+        if (!empty($_POST['user_login'])) {
             $userLogin = test_input($_POST['user_login']);
-            $userPassword = test_input($_POST['user_password']);
-
-            // enregistrement en base
+            if (mb_strlen($userLogin) < 5 || mb_strlen($userLogin) > 30) {
+                $errors['user_login'] = 'De 5 à 30 caractères';
+            }
         } else {
-            // creer messages d'erreurs
+            $errors['user_login'] = 'Ne pas laisser vide';
+        }
+        if (!empty($_POST['user_password'])) {
+            $userPassword = test_input($_POST['user_password']);
+            if (mb_strlen($userPassword) < 7 || mb_strlen($userPassword) > 15) {
+                $errors['user_password'] = 'De 7 à 15 caractères';
+            }
+        } else {
+            $errors['user_password'] = 'Ne pas laisser vide';
         }
     // traitement form sign in
     } elseif (isset($_POST['sign_in'])) {
         echo'Sign in';
     }
 }
-?>
 
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= SITE_NAME ?></title>
+    <title><?= SITE_NAME ?>: Accueil</title>
     <link rel="stylesheet" href="../assets/css/main.css">
 </head>
 <body>
@@ -37,7 +46,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <h2>Je n'ai pas encore de compte :</h2>
             <form action="" method="POST">
                 <input type="text" name="user_login" placeholder="JeanClaudeDuss">
+                <?php if (!empty($errors['user_login'])): ?>
+                    <p><?= $errors['user_login'] ?></p>
+                <?php endif ?>
                 <input type="password" name="user_password">
+                <?php if (!empty($errors['user_password'])): ?>
+                    <p><?= $errors['user_password'] ?></p>
+                <?php endif ?>
                 <input type="submit" name="sign_up" value="M'inscrire">
             </form>
         </section>
