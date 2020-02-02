4bfc21e84ce19c2b5a66702f079416fe94899887
quentin.picart <quentin.picart@gmail.com>
2020-02-02 11:14:17 +0100

    details: modification mise en page et style

--- src/details.php
+++ src/details.php
@@ -52,34 +52,36 @@ if (!empty($_GET['id'])) {
         <?php if($detailsVaisseau): ?>
         <h1>Détails du vaisseau: <?= $detailsVaisseau->nomVaisseau ?></h1>
         <div id="details-vaisseau">
-            <section>
-                <h1>Statistiques du vaisseau</h1>
-                <table>
-                    <thead>
-                        <th>Attaque</th>
-                        <th>Défense</th>
-                        <th>Rapidité</th>
-                        <th>Solidité</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><?= $detailsVaisseau->attaque ?></td>
-                            <td><?= $detailsVaisseau->defense ?></td>
-                            <td><?= $detailsVaisseau->rapidite ?></td>
-                            <td><?= $detailsVaisseau->solidite ?></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </section>
+            <div>
+                <section>
+                    <h1>Statistiques du vaisseau</h1>
+                    <table>
+                        <thead>
+                            <th>Attaque</th>
+                            <th>Défense</th>
+                            <th>Rapidité</th>
+                            <th>Solidité</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?= $detailsVaisseau->attaque ?></td>
+                                <td><?= $detailsVaisseau->defense ?></td>
+                                <td><?= $detailsVaisseau->rapidite ?></td>
+                                <td><?= $detailsVaisseau->solidite ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+                <section>
+                    <h1>Type du vaisseau</h1>
+                    <p>Vaisseau de type: <?= $detailsVaisseau->nomType ?></p>
+                    <p><?= $detailsVaisseau->detail ?></p>
+                </section>
+            </div>
             <div>
                 <img src="<?=$detailsVaisseau->lienImage?>" alt="">
             </div>
         </div>
-        <section>
-            <h1>Type du vaisseau</h1>
-            <p>Vaisseau de type: <?= $detailsVaisseau->nomType ?></p>
-            <p><?= $detailsVaisseau->detail ?></p>
-        </section>
         <?php else: ?>
         <h1>Pas de vaisseau correspondant !</h1>
         <?php endif ?>
