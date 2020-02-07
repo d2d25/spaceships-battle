737ebe754e572ef0ed6ab619be6252c0dadb34aa
d2d25 <57818560+d2d25@users.noreply.github.com>
2020-02-07 20:11:39 +0100

    changement de jouer-vaisseau

--- includes/connect_base.php
+++ includes/connect_base.php
@@ -1,8 +1,10 @@
 <?php
 /* Connexion à une base MySQL avec l'invocation de pilote */
+include "connect_infos.php";
 try {
-    $pdo = new PDO(DSN, USER, PASSWORD);
+    $pdo = new PDO("mysql:host=$server;dbname=$base", $user, $mdp);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo '<script>console.log("connexion reussie")</script>';
 } catch (PDOException $e) {
     echo 'Connexion échouée : ' . $e->getMessage();
 }
--- includes/connect_infos.env.php
+++ includes/connect_infos.env.php
@@ -1,4 +1,5 @@
 <?php
-define('DSN','mysql:dbname=testdb;host=127.0.0.1;charset=utf8');
-define('USER','db_user');
-define('PASSWORD','db_password');
+$server = '127.0.0.1';
+$user = 'root';
+$mdp = '';
+$base = 'DBNom';
